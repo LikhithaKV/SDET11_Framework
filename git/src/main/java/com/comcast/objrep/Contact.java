6d5983eec50e00bed06062b253a2e08c548a0839
LENOVO <LENOVO@DESKTOP-2L9NM0M>
2021-03-15 13:49:18 +0530

    updating new method by lead

--- git/src/main/java/com/comcast/objrep/Contact.java
+++ git/src/main/java/com/comcast/objrep/Contact.java
@@ -1,9 +1,14 @@
 package com.comcast.objrep;
 
 public class Contact {
-  // junior//
+	 /**
+	   * @user1
+	   */
 	public void createContact() {
 		
 	}
 
+public void createContact(String contName, long mobileNum) {
+		
+	}
 }
