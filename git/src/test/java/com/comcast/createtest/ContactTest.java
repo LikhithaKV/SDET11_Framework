f462ed6f675d23329eb4b9fc69320552ab2212bc
LENOVO <LENOVO@DESKTOP-2L9NM0M>
2021-03-15 12:55:50 +0530

    pushing to local rep

---
+++ git/src/test/java/com/comcast/createtest/ContactTest.java
@@ -0,0 +1,22 @@
+package com.comcast.createtest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createcontacttest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifycontacttest");
+	}
+	
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deletecontacttest");
+	}
+
+}
